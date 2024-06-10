c565330054088d3598a677e9385518641123e8a5
Pam-123 <pamschawira@gmail.com>
2024-06-10 11:54:58 -0700

    Yo

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""
+
+"""
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
 from models import storage
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+@app_views.route('/status')
+def status():
+    """return status in json format"""
+    s = {"status": "OK"}
+    return jsonify(s)
 
 
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+@app_views.route('/stats')
+def stats():
+    """ """
+    classes = {"amenities": "Amenity", "cities": "City",
+               "places": "Place", "reviews": "Review",
+               "states": "State", "users": "User"}
+    retval = {}
+    for key, val in classes.items():
+        retval[key] = storage.count(val)
+    return jsonify(retval)
